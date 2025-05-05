3d696aebfe9a0dffe9516987dc02a77273835c9f
bandit-raccoon <joypirate13@yahoo.com>
2025-05-05 15:54:11 +0530

    Basic Greet using Name

---
+++ boilerPlate.py
@@ -0,0 +1,9 @@
+def greet(name):
+    """
+    This function takes a name as input and prints a greeting message.
+    
+    :param name: str - The name of the person to greet
+    """
+    print(f"Hello, {name}!")
+
+name = str(input("Enter your name: "))
